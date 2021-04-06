152951c54883ed7a3bc58e9404adfd10f15fd62c
Murithi paul <murithipaul1@gmail.com>
2021-04-06 11:36:39 +0300

    more configurations

--- deploy_example/gunicorn.conf.py
+++ deploy_example/gunicorn.conf.py
@@ -1,8 +1,35 @@
 import multiprocessing
 
-#for development. Remove this line in production
+# name
+proc_name = 'deploy_example'
+user = 'programmer'
+group = 'programmer'
+
+#for development. Remove this lines in production
 bind="127.0.0.1:8000"
-workers=multiprocessing.cpu_count() * 2 + 1
-# forwarded-allow-ips="*"
-# log-level=debug
 reload=True
+
+workers=multiprocessing.cpu_count() * 2 + 1
+
+forwarded_allow_ips = '*'
+    
+# headers used by the front-end proxy to indicate HTTPS
+secure_scheme_headers = {
+    'X-FORWARDED-PROTOCOL': 'ssl', 
+    'X-FORWARDED-PROTO': 'https', 
+    'X-FORWARDED-SSL': 'on'
+}
+
+# logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'debug'
+
+# ssl keyfile
+keyfile = ''
+
+# ssl certile
+certfile = ''
+
+# ssl version to use
+ssl_version = 'TLS'
--- deploy_example/gunicorn.sh
+++ deploy_example/gunicorn.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
 # declare variables
-NAME="deploy_example"
 HOMEDIR=/home/programmer/djangoprojects/deploy-jango/
 GUNICORN=/home/programmer/.local/share/virtualenvs/deploy-jango-AxTjPbqZ/bin/gunicorn
 SOCKFILE=/home/programmer/.local/share/virtualenvs/deploy-jango-AxTjPbqZ/run/gunicorn.sock
-USER=programmer
-GROUP=programmer
-# NUM_WORKERS=2
 DJANGO_SETTINGS_MODULE=deploy_example.settings
 PYTHONPATH=/home/programmer/.local/share/virtualenvs/deploy-jango-AxTjPbqZ/bin/python
 DJANGO_WSGI_MODULE=deploy_example.wsgi
@@ -25,11 +21,4 @@ RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
 
 # start gunicorn
-echo "Starting $NAME as `whoami`"
-exec $GUNICORN $DJANGO_WSGI_MODULE:application \
-    --name=$NAME \
-    # --workers=$NUM_WORKERS \
-    --user=$USER --group=$GROUP \
-    --log-level=debug \
-    --bind=unix:$SOCKFILE \
-    --forwarded-allow-ips="*"
+exec $GUNICORN $DJANGO_WSGI_MODULE:application # --bind=unix:$SOCKFILE 
