ec265496896de7ea39fb3c0f58cc7d638089fe65
Murithi paul <murithipaul1@gmail.com>
2021-04-06 09:31:56 +0300

    initial commit

---
+++ deploy_example/gunicorn.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# declare variables
+NAME="deploy_example"
+HOMEDIR=/home/programmer/djangoprojects/deploy-jango/
+GUNICORN=/home/programmer/.local/share/virtualenvs/deploy-jango-AxTjPbqZ/bin/gunicorn
+SOCKFILE=/home/programmer/.local/share/virtualenvs/deploy-jango-AxTjPbqZ/run/gunicorn.sock
+USER=programmer
+GROUP=programmer
+NUM_WORKERS=4
+DJANGO_SETTINGS_MODULE=deploy_example.settings
+PYTHONPATH=/home/programmer/.local/share/virtualenvs/deploy-jango-AxTjPbqZ/bin/python
+DJANGO_WSGI_MODULE=deploy_example.wsgi
+
+
+# activate virtual environment
+cd $HOMEDIR
+pipenv shell
+
+cd $HOMEDIR/deploy_example
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$PYTHONPATH
+
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# start gunicorn
+echo "Starting $NAME as `whoami`"
+exec $GUNICORN $DJANGO_WSGI_MODULE:application \
+    --name $NAME --workers $NUM_WORKERS \
+    --user=$USER --group=$GROUP \
+    --log-level=info \
+    --bind=unix:$SOCKFILE \
+    --forwarded-allow-ips="*"
